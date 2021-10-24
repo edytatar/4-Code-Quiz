e604d6f2e7557d7b2c3606a3c0234cb524584e24
edytatar <edytatar@gmail.com>
2021-10-24 16:47:30 -0500

    Adding JS file for end.html page

---
+++ Assets/JavaScript/end.js
@@ -0,0 +1,36 @@
+// Declaring variables
+var username = document.querySelector("#username");
+var saveScoreBtn = document.querySelector("#saveScoreBtn");
+var finalScore = document.querySelector("#finalScore");
+var mostRecentScore = localStorage.getItem("mostRecentScore");
+
+// saving highscore
+var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+// total amount of highscores that will display on page
+var maxHighScores = 5;
+
+// changing text in final score area to the most recent score
+finalScore.innerText = mostRecentScore;
+
+// function that is invoked when "save" button is clicked
+saveHighScore = e => {
+    //Prevents default actions when button is clicked
+    e.preventDefault();
+
+    var score = {
+        score: mostRecentScore,
+        name: username.value
+    }
+
+    highScores.push(score);
+
+    highScores.sort((a, b) => {
+        return b.score - a.score;
+    })
+
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("index.html");
+}
